d18783c87433f705b0bc99fb8066e680ea5e919c
rafi <rafialam6610@gmail.com>
2025-05-26 23:14:14 +0600

    [LOGS] Add logs to find out CORS error

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,12 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((req, res, next) => {
+        console.log(`Incoming Request Origin: ${req.headers.origin}`);
+        next();
+    });
+    const corsOrigin = process.env.REQUEST_ORIGIN;
+    console.log(`CORS Origin: ${corsOrigin}`);
     app.enableCors({
         origin: process.env.REQUEST_ORIGIN,
         methods: 'GET,OPTIONS,PUT,PATCH,POST,DELETE',
